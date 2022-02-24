5329668b5cb9da6983ec4921257567d971858ba5
Liusirui <93499354+HelloThreeLiu@users.noreply.github.com>
2022-02-24 15:11:34 +0800

    上传笔记

---
+++ Javaearning/collection-map-app2/src/com/itlsr/d1_collection_set/SetDemo2.java
@@ -0,0 +1,14 @@
+package com.itlsr.d1_collection_set;
+
+public class SetDemo2 {
+    public static void main(String[] args) {
+        // 目标：学会获取对象的哈希值，并确认一下
+        String name = "itliusr";
+        System.out.println(name.hashCode());
+        System.out.println(name.hashCode());
+
+        String name1 = "itliusr1";
+        System.out.println(name1.hashCode());
+        System.out.println(name1.hashCode());
+    }
+}
